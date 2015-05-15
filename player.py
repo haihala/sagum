db0088239dd9edcc7523c35f3a2c91bc8827b6ff
Häihälä <eero.haihala@gmail.com>
2015-05-15 19:35:52 +0300

    ´robust player and almost done server

---
+++ player.py
@@ -0,0 +1,6 @@
+class Player():
+    def __init__(self, name, ip, x, y):
+        self.ip = ip
+        self.name = name
+        self.x = x
+        self.y = y
--- server.py
+++ server.py
@@ -2,9 +2,12 @@ import socket
 import ctypes
 import sys
 import time
+import threading
+import player
 
-entities = [] # a list containing all the entities of the game.
-players = []
+
+entities = [] # a list containing all the entities currently in the game.
+players = []  # -"- players in the game
 socket.setdefaulttimeout(0.05)
 
 def start():
@@ -14,36 +17,49 @@ def start():
     tell("Firing up ye ol' genny to produce some entities.")
     entities = generateEntities()
     tell("Entities generated, trying to open sockets. Sockets have nothing to do with socks, which is confusing.")
+    global s, r
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Sending socket
 
     r = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Receiving socket (same socket can not listen and send)
     r.bind(("127.0.0.1", 9001)) # Server is listening port 9001 and sending on 9002. Ip is localhost for obvious reasons.
     tell("Found my old lasso and bound port 9001 to listening mode.")
 
+    global consoleThread
+    consoleThread.daemon = True
+    consoleThread.start()
+    tell("Started to knit and started a new thread with console handeling.")
+
     tell("Preparing to run like Usain Bolt.")
     runLoop()
 
 def runLoop():
+    """The main loop of the server. It is managing ticks and messaging. In case of a 'quit' command it shuts down the server."""
     global entities
     global players
-    lastsec = time.time()
+    lastsec = lasttick = time.time()
     tickCount = 0
+    skipticks = 0
     tell("Running.")
+    global kill
     while 1:
+        if (kill):
+            sys.exit(2)
+
         if lastsec + 1 <= time.time():
             lastsec = time.time()
             if (tickCount < 20):
-                tell("skipped " + str(20 - tickCount) + " ticks this second")
+                tell("skipped " + str(20 - tickCount) + " ticks this second.")
             tickCount = 0
 
-        if (tickCount >= 20):
-            time.sleep(0.01)
-        else:
+        if lasttick + 0.047 < time.time() and tickCount <= 20:
+            lasttick = time.time()
             tickCount += 1
             tick()
 
 def tick():
-    pass
+    """One update that is supposed to make the game go forwards. Happens about 20 times per second"""
+    for i in range(100000): # test loop to find out what can a normal pc do in 1/20 s.
+        a = 100000**100000
 
 def tell(message):
     """A nicer print, determines the module printing the message."""
@@ -55,20 +71,35 @@ def tell(message):
 
 def sendAll(msg):
     """Send message to all clients."""
-    pass
+    global players
+    global s
+    for p in players:
+        s.sendto("msg", p.ip)
 
 def generateEntities():
     """Fill server entities list with items, players, mobs, etc."""
     return []
 
-def consolehandler():
-    while 1:
-        inp = raw_input(">")
-        inp = inp.lowercase
-        inp = inp[0]
-        if inp == "h":
-            tell("Help is on the way, just hold on!")
 
+def consolehandler():
+    """This function is ran on a thread. It keeps the console running."""
+    time.sleep(1)
+    global kill
+    while 1:
+        if (kill):
+            break
+
+        inp = raw_input(">")
+        inp = inp.lower()
+        inp = inp.split()
+        i = inp[0][0]
+        if i == "h":
+            tell("Help is on its way, just hold on!")
+        elif i == "q":
+            tell("Are you sure you want to quit? (y/n)")
+            if (raw_input(">")[0].lower() == "y"):
+                tell("Shutting down...")
+                kill = True
 
 if __name__ == "__main__":
     if(ctypes.windll.shell32.IsUserAnAdmin()==0):
@@ -77,9 +108,12 @@ if __name__ == "__main__":
         sys.exit(0)
     else:
         tell("User is very supery and quite administrative")
-    tell(str(time.time()))
+
+    consoleThread = threading.Thread(target=consolehandler)
+
+    kill = False
     start()
-    
+
 """
 import socket
 import ctypes
